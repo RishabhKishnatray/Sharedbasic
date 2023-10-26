b47f3fc89d5910970bddd13943f3398ddfbb2325
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-10-26 13:38:57 +0530

    Update commentFirst.groovy

--- vars/commentFirst.groovy
+++ vars/commentFirst.groovy
@@ -1,3 +1,8 @@
 def call() {
-    sh 'echo hii this is first shared library'
+    sh '''
+    sudo apt update
+sudo apt install python3-pip -y
+sudo pip install boto3 botocore
+sudo pip3 install ansible==2.9.6
+'''
 }
