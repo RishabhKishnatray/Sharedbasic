0a89419639493a2fa7452b1152f6109435f78b4e
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-10-26 13:45:26 +0530

    Update commentFirst.groovy

--- vars/commentFirst.groovy
+++ vars/commentFirst.groovy
@@ -1,8 +1,8 @@
 def call() {
     sh '''
     sudo apt update
-sudo apt -y install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt install ansible -y
+sudo apt install python3-pip -y
+sudo pip install boto3 botocore
+sudo pip3 install ansible==2.9.6
 '''
 }
